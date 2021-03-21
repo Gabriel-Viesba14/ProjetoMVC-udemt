d4423d9bfa3f41dd2404733d28aaa57fe596d21e
gabriel <gabriel.viesba14@gmail.com>
2021-03-21 17:24:48 -0300

    criação do projeto MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoMVC_udemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
